ea4a4ea67c86d5b3ddbdb9a1c9a52f410711a37c
JasperFang <jasper20467@hotmail.com>
2020-01-11 16:09:42 +0800

    Init MysqlStockFilter upload

---
+++ MysqlStockFilter/Connection/ConnectConfig.py
@@ -0,0 +1,7 @@
+import datetime 
+import pymysql
+import pandas as pd
+
+class ConnectString():
+    def __init__(self):
+        self.MysqlConn = ''
---
+++ MysqlStockFilter/program.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import datetime
+import sys
+import Connection.ConnectConfig
+
+
+class main():
+    print('Hello Richer')
+
